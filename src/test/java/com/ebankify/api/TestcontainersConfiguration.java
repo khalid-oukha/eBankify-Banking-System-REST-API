097c689647c1523b9c3d483927888e4a85624815
khalidwaheli <oukhakhalid@gmail.com>
2024-12-19 20:35:17 +0100

    sonar qub added

--- src/test/java/com/ebankify/api/TestcontainersConfiguration.java
+++ src/test/java/com/ebankify/api/TestcontainersConfiguration.java
@@ -1,23 +1,8 @@
 package com.ebankify.api;
 
 import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-import org.springframework.context.annotation.Bean;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
 class TestcontainersConfiguration {
-
-    @Bean
-    @ServiceConnection
-    PostgreSQLContainer<?> postgresContainer() {
-        return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"))
-                .withDatabaseName("testdb")
-                .withUsername("test")
-                .withPassword("test")
-                .withReuse(true)
-                .withExposedPorts(5432);
-    }
 
 }
